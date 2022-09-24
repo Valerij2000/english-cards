3fbf38fd7f35245bd94d08fd27d647bd736703a8
Валерий Шумков <valerijsumkov@MacBook-Pro-Valerij.local>
2022-09-24 20:15:32 +0400

    add admin panel

---
+++ js/login.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+  let user = document.querySelector('[name="user"]'),
+    pass = document.querySelector('[name="pass"]');
+  const button = document.querySelector('[data-button]');
+  button.addEventListener('click', () => {
+    if (user.value !== '' && user.value === 'marina' && pass.value !== '' && pass.value == 123) {
+      document.querySelector('.error').innerHTML = '';
+      window.location.href = 'public/index.html';
+    } else {
+      document.querySelector('.error').innerHTML = 'Form invalid';
+    }
+  })
+})
