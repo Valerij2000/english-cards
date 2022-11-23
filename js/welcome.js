b310d54aed7db9db0053dd2ee86d6a6992a71e9d
Валерий Шумков <valerijsumkov@MacBook-Pro-Valerij.local>
2022-11-24 00:05:59 +0300

    added page phrases

--- js/welcome.js
+++ js/welcome.js
@@ -4,19 +4,33 @@ const card = document.querySelector(".card");
 const link = document.querySelector('[data-link-admin]');
 const buttonTemplateVords = document.querySelector('[button-template]');
 const buttonUniqeVords = document.querySelector('[button-uniqe]');
+const buttonPhrases = document.querySelector('[button-phrases]');
 const containerUniqe = document.querySelector('[data-uniqe-vords]');
 const containerTemplate = document.querySelector('[data-template-vords]');
+const containerPhrases = document.querySelector('[data-phrases-vords]');
 buttonTemplateVords.addEventListener('click', function() {
     this.classList.add('active-choice');
     buttonUniqeVords.classList.remove('active-choice');
+    buttonPhrases.classList.remove('active-choice');
     containerTemplate.classList.remove('hidden');
     containerUniqe.classList.add('hidden');
+    containerPhrases.classList.add('hidden');
 })
 buttonUniqeVords.addEventListener('click', function() {
     this.classList.add('active-choice');
     buttonTemplateVords.classList.remove('active-choice');
+    buttonPhrases.classList.remove('active-choice');
     containerUniqe.classList.remove('hidden');
     containerTemplate.classList.add('hidden');
+    containerPhrases.classList.add('hidden');
+})
+buttonPhrases.addEventListener('click', function() {
+    this.classList.add('active-choice');
+    buttonUniqeVords.classList.remove('active-choice');
+    buttonTemplateVords.classList.remove('active-choice');
+    containerPhrases.classList.remove('hidden');
+    containerTemplate.classList.add('hidden');
+    containerUniqe.classList.add('hidden');
 })
 const handleButtonClick = (e) => {
     const targetSection = e.target.getAttribute("data-section");
