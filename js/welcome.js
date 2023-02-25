4bc547b6a643e332285909d23d6a7fcb86c89c98
Валерий Шумков <valerijsumkov@MacBook-Pro-Valerij.local>
2023-02-25 14:42:24 +0400

    added new admin parts

--- js/welcome.js
+++ js/welcome.js
@@ -8,43 +8,61 @@ const buttonPhrases = document.querySelector('[button-phrases]');
 const containerUniqe = document.querySelector('[data-uniqe-vords]');
 const containerTemplate = document.querySelector('[data-template-vords]');
 const containerPhrases = document.querySelector('[data-phrases-vords]');
-buttonTemplateVords.addEventListener('click', function() {
-    this.classList.add('active-choice');
-    buttonUniqeVords.classList.remove('active-choice');
-    buttonPhrases.classList.remove('active-choice');
-    containerTemplate.classList.remove('hidden');
-    containerUniqe.classList.add('hidden');
-    containerPhrases.classList.add('hidden');
+const captionCounterAdminList = document.querySelectorAll('[data-uniqe-vords] .card-item-title');
+const adminButtonLinks = document.querySelectorAll('.card-admin-link');
+buttonTemplateVords.addEventListener('click', function () {
+	this.classList.add('active-choice');
+	buttonUniqeVords.classList.remove('active-choice');
+	buttonPhrases.classList.remove('active-choice');
+	containerTemplate.classList.remove('hidden');
+	containerUniqe.classList.add('hidden');
+	containerPhrases.classList.add('hidden');
 })
-buttonUniqeVords.addEventListener('click', function() {
-    this.classList.add('active-choice');
-    buttonTemplateVords.classList.remove('active-choice');
-    buttonPhrases.classList.remove('active-choice');
-    containerUniqe.classList.remove('hidden');
-    containerTemplate.classList.add('hidden');
-    containerPhrases.classList.add('hidden');
+buttonUniqeVords.addEventListener('click', function () {
+	this.classList.add('active-choice');
+	buttonTemplateVords.classList.remove('active-choice');
+	buttonPhrases.classList.remove('active-choice');
+	containerUniqe.classList.remove('hidden');
+	containerTemplate.classList.add('hidden');
+	containerPhrases.classList.add('hidden');
 })
-buttonPhrases.addEventListener('click', function() {
-    this.classList.add('active-choice');
-    buttonUniqeVords.classList.remove('active-choice');
-    buttonTemplateVords.classList.remove('active-choice');
-    containerPhrases.classList.remove('hidden');
-    containerTemplate.classList.add('hidden');
-    containerUniqe.classList.add('hidden');
+buttonPhrases.addEventListener('click', function () {
+	this.classList.add('active-choice');
+	buttonUniqeVords.classList.remove('active-choice');
+	buttonTemplateVords.classList.remove('active-choice');
+	containerPhrases.classList.remove('hidden');
+	containerTemplate.classList.add('hidden');
+	containerUniqe.classList.add('hidden');
 })
 const handleButtonClick = (e) => {
-    const targetSection = e.target.getAttribute("data-section");
-    const section = document.querySelector(targetSection);
-    targetSection !== "#about"
-        ? card.classList.add("is-active")
-        : card.classList.remove("is-active");
-    card.setAttribute("data-state", targetSection);
-    sections.forEach((s) => s.classList.remove("is-active"));
-    buttons.forEach((b) => b.classList.remove("is-active"));
-    e.target.classList.add("is-active");
-    section.classList.add("is-active");
+	const targetSection = e.target.getAttribute("data-section");
+	const section = document.querySelector(targetSection);
+	targetSection !== "#about" ?
+		card.classList.add("is-active") :
+		card.classList.remove("is-active");
+	card.setAttribute("data-state", targetSection);
+	sections.forEach((s) => s.classList.remove("is-active"));
+	buttons.forEach((b) => b.classList.remove("is-active"));
+	e.target.classList.add("is-active");
+	section.classList.add("is-active");
 };
 buttons.forEach((btn) => {
-    btn.addEventListener("click", handleButtonClick);
+	btn.addEventListener("click", handleButtonClick);
 });
 link.addEventListener('click', () => window.location.href = './public/index.html');
+captionCounterAdminList.forEach((el, index) => {
+	let counter = index + 1;
+	if (counter < 10) {
+		el.innerHTML = `Part 0${counter}`;
+	} else {
+		el.innerHTML = `Part ${counter}`;
+	}
+
+})
+adminButtonLinks.forEach((el, index) => {
+	if (index === 0) {
+		el.href = `./admin/vords.html`;
+	} else {
+		el.href = `./admin/vords0${index}.html`;
+	}
+})
